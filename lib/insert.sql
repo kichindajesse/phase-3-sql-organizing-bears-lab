77c34047c7da36ad08eaf44eed983c2014900242
kichinda <53686544+kichinda@users.noreply.github.com>
2023-02-20 08:59:49 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,21 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
+
+
+INSERT INTO bears (name, age, gender, color) VALUES ('Yogi', 10, 'male', 'brown');
+INSERT INTO bears (name, age, gender, color) VALUES ('Paddington', 5, 'male', 'brown');
+INSERT INTO bears (name, age, gender, color) VALUES ('Winnie', 20, 'female', 'yellow');
+INSERT INTO bears (name, age, gender, color) VALUES ('Baloo', 15, 'male', 'gray');
+INSERT INTO bears (name, age, gender, color) VALUES ('Gummy', 7, 'female', 'purple');
+INSERT INTO bears (name, age, gender, color) VALUES (NULL, 2, 'male', 'white');
+INSERT INTO bears (name, age, gender, color) VALUES ('Koda', 3, 'male', 'blue');
+INSERT INTO bears (name, age, gender, color) VALUES ('Fozzie', 8, 'male', 'orange');
+
+
